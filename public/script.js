50e9398dcfd1e14f715581c2ef26b621fd68e685
michaelbzhu <mbzhu1@gmail.com>
2021-02-15 15:42:35 -0800

    meem refactor

--- public/script.js
+++ public/script.js
@@ -5,50 +5,6 @@ canvas.setWidth(document.body.scrollWidth);
 fabric.Object.prototype.originX = true;
 fabric.Object.prototype.originY = true;
 
-const meems = [
-  "https://pyxis.nymag.com/v1/imgs/258/ab8/b37c0379b29ab114b7c18e2bc11e89a5ec-the-weeknd-meme.jpg",
-  "https://images.dailyhive.com/20210208113102/The-Weeknd-Super-Bowl-meme-500x258.jpg",
-  "https://images.vice.com/motherboard/content-images/contentimage/10791/1399664177814.jpeg?crop=0.9965277777777778xw:1xh;center,center&resize=1200:*",
-  "https://images.news18.com/ibnlive/uploads/2021/02/1613011996_untitled-design.jpg",
-  "https://mk0coinnouncemdktlrl.kinstacdn.com/wp-content/uploads/2019/10/dogecoin-price-prediction-2020.jpg",
-  "https://s.yimg.com/ny/api/res/1.2/VbcLEYqGOCYf9c6Xqu5y5Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTk2Ni45NTY1MjE3MzkxMzA0/https://s.yimg.com/uu/api/res/1.2/v.dZ42vjxY8wwDJAVurvig--~B/aD04MzQ7dz04Mjg7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/coindesk_75/0af2f44c8622a758f80a7aa185b4a9e0",
-  "https://i.redd.it/kjvdc916y6051.jpg",
-  "https://compote.slate.com/images/926e5009-c10a-48fe-b90e-fa0760f82fcd.png?width=1200&rect=680x453&offset=0x30",
-  "https://starecat.com/content/wp-content/uploads/stonks-only-go-up-wheel-of-fortune-meme.jpg",
-  "https://i.pinimg.com/originals/e1/8e/79/e18e79774e2f5ec7bc61100c2c6ed0f3.png",
-  "https://art.ngfiles.com/images/1085000/1085516_superwiibros08_untitled-goose-game-meme.png?f1574025751",
-  "https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F018%2F489%2Fnick-young-confused-face-300x256-nqlyaa.jpg",
-  "https://i.imgflip.com/2m16fo.jpg",
-  "https://i.imgflip.com/1ur9b0.jpg",
-  "https://i.imgflip.com/1otk96.jpg",
-  "https://i.imgflip.com/28j0te.jpg",
-  "https://i.imgflip.com/1c1uej.jpg",
-  "https://i.imgflip.com/4acd7j.png",
-  "https://i.imgflip.com/1ihzfe.jpg",
-  "https://i.imgflip.com/26am.jpg",
-  "https://i.imgflip.com/1bij.jpg",
-  "https://i.imgflip.com/1h7in3.jpg",
-  "https://i.imgflip.com/2896ro.jpg",
-  "https://i.imgflip.com/2wifvo.jpg",
-  "https://i.imgflip.com/wxica.jpg",
-  "https://i.imgflip.com/39t1o.jpg",
-  "https://i.imgflip.com/gtj5t.jpg",
-  "https://i.imgflip.com/4t0m5.jpg",
-  "https://i.imgflip.com/1e7ql7.jpg",
-  "https://i.imgflip.com/1bgw.jpg",
-  "https://i.imgflip.com/2hgfw.jpg",
-  "https://i.imgflip.com/8p0a.jpg",
-  "https://i.imgflip.com/9sw43.jpg",
-  "https://i.imgflip.com/3si4.jpg",
-  "https://i.imgflip.com/51s5.jpg",
-  "https://i.imgflip.com/1bim.jpg",
-  "https://i.redd.it/2ilq6b6qwbh61.jpg",
-  "https://preview.redd.it/xnjv1isufbh61.jpg?width=640&crop=smart&auto=webp&s=f3b3d36de914d42dc16810c93123f9c4e12b8d75",
-  "https://preview.redd.it/1su9be8duch61.jpg?width=640&crop=smart&auto=webp&s=496096d83b0b89d3b20627b6d785df6d8ec9dfb3",
-  "https://i.redd.it/mr27ocesqbh61.png",
-  "https://preview.redd.it/6q329lw63eh61.jpg?width=640&crop=smart&auto=webp&s=999d4da2c38f403f55536469f40bd173f70ae20d",
-];
-
 document.getElementById("decorateButton").addEventListener("click", () => {
   const imageURL = document.getElementById("imageURL");
   addImage(imageURL.value);
@@ -62,8 +18,8 @@ document.getElementById("surpriseButton").addEventListener("click", () => {
 function addImage(url) {
   console.log(url);
   fabric.Image.fromURL(url, (img) => {
-    scaleX = 400;
-    scaleY = 300;
+    scaleX = 250;
+    scaleY = 250;
     x = Math.floor(Math.random() * canvas.getWidth()) + scaleX / 2;
     y = Math.floor(Math.random() * canvas.getHeight()) + scaleY / 2;
     console.log(x, y);
