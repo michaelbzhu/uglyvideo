5412abf752894832b3990523fe8005e6e3c4a487
Andrew Shen <andrewshen@gmail.com>
2022-08-29 19:48:01 -0700

    Add draggable video

--- public/script.js
+++ public/script.js
@@ -15,6 +15,50 @@ document.getElementById("surpriseButton").addEventListener("click", () => {
   addImage(url);
 });
 
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
 function addImage(url) {
   console.log(url);
   fabric.Image.fromURL(url, (img) => {
@@ -257,6 +301,7 @@ function addVideoStream(video, stream) {
     video.play();
   });
   // add video element to our videogrid
+  dragElement(video);
   videoGrid.append(video);
 }
 
